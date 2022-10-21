5bd9ae1967c4518d99c18e2f0931f443cb8fc2f7
CristianJ77 <cristianfigueroa489@gmail.com>
2022-10-20 20:52:26 -0600

    SideBar y sus componentes agregados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'auth',
-    loadChildren:() => import(`./modules/auth/auth.module`).then(m => m.AuthModule)
+    path: 'home',
+    loadChildren:() => import(`./modules/home/home.module`).then(m => m.HomeModule)
   },
 ];
 
